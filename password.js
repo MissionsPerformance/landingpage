d2bf07a67e34dcfcea0fd619183ff0da28edbe59
Michael Ibragimchayev <michael@chayev.com>
2025-06-26 10:21:50 -0400

    Create password.js

---
+++ password.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const password = "2025June27UN";
+
+    const modal = document.getElementById('password-modal');
+    const passwordForm = document.getElementById('password-form');
+    const passwordInput = document.getElementById('password-input');
+    const mainContent = document.querySelector('main');
+    const header = document.querySelector('.header');
+    const footer = document.querySelector('.footer');
+
+
+    // Obfuscate the page content initially
+    if(mainContent) mainContent.style.display = 'none';
+    if(header) header.style.display = 'none';
+    if(footer) footer.style.display = 'none';
+
+
+    passwordForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        if (passwordInput.value === password) {
+            modal.style.display = 'none';
+            if(mainContent) mainContent.style.display = 'block';
+            if(header) header.style.display = 'block';
+            if(footer) footer.style.display = 'block';
+
+        } else {
+            alert("Incorrect password. Please try again.");
+            passwordInput.value = '';
+        }
+    });
+});
